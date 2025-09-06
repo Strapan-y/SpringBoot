2a42ff52da8a1867219d536c4a63429f3c640ef6
strapan-y <andreacorrea10820@gmail.com>
2025-09-06 18:22:42 -0500

    Primer commit: subiendo proyecto SpringBoot

---
+++ src/main/java/com/andreastrap/strapan_y/product_backend/product/BackendDeProductoSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.andreastrap.strapan_y.product_backend.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeProductoSprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeProductoSprintBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreastrap/strapan_y/product_backend/product/BackendDeProductoSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreastrap.strapan_y.product_backend.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeProductoSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
